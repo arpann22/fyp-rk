84264d36bc60f84a0b4306a5bec5479d91b54214
arpann22 <71937861+arpann22@users.noreply.github.com>
2023-05-23 16:58:55 +0545

    Add files via upload

---
+++ fyp/faq.php
@@ -0,0 +1,98 @@
+<?php include_once("includes/basic_includes.php"); ?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Find Your Perfect Partner - Matrimony | Faq :: Matrimony
+	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<script
+		type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Custom Theme files -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+	<!----font-Awesome----->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!----font-Awesome----->
+	<script>
+		$(document).ready(function () {
+			$(".dropdown").hover(
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideDown("fast");
+					$(this).toggleClass('open');
+				},
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideUp("fast");
+					$(this).toggleClass('open');
+				}
+			);
+		});
+	</script>
+</head>
+
+<body>
+	<!-- ============================  Navigation Start =========================== -->
+	<?php include_once("includes/navigation.php"); ?>
+	<!-- ============================  Navigation End ============================ -->
+	<div class="grid_3">
+		<div class="container">
+			<div class="breadcrumb1">
+				<ul>
+					<a href="index.php"><i class="fa fa-home home_1"></i></a>
+					<span class="divider">&nbsp;|&nbsp;</span>
+					<li class="current-page">FAQ</li>
+				</ul>
+			</div>
+			<dl class="faq-list">
+				<dt class="faq-list_h">
+					<b>1.How do I create an account on the matrimony site?</b> <br>
+
+					To create an account, visit the matrimony site's homepage and look for the "Sign Up" or "Register"
+					button. Click on it and follow the prompts to provide your personal details, such as name, email
+					address, and contact information. You may also be required to create a username and password for
+					login purposes. <br><br>
+
+					<b>2.How can I search for potential matches on the matrimony site?</b><br>
+
+					Most matrimony sites provide search options based on various criteria, including age, religion,
+					caste, education, occupation, and more. Look for the "Search" or "Find Matches" feature on the site
+					and enter your desired preferences. You can further refine your search results by using additional
+					filters to find profiles that closely match your requirements.<br><br>
+
+					<b>3.How can I ensure the authenticity and genuineness of profiles on the matrimony site?</b><br>
+
+					Matrimony sites employ various measures to verify the authenticity of profiles. Look for sites that
+					have robust verification processes, such as email verification, phone number verification, or even
+					identity document verification. Additionally, read user reviews and testimonials to gauge the
+					reputation and credibility of the matrimony site.<br><br>
+
+					<b>4.Can I communicate with potential matches directly on the matrimony site?</b><br>
+
+					Yes, most matrimony sites provide communication features that allow you to interact with potential
+					matches. These features may include messaging options, chat rooms, or even video calling facilities.
+					Once you find a profile that interests you, you can initiate communication through the available
+					channels to get to know each other better.<br><br>
+
+					<b>5.What steps should I take for privacy and security on the matrimony site?</b><br>
+
+					Matrimony sites take user privacy and security seriously. However, it's essential to take personal
+					precautions as well. Ensure you read and understand the site's privacy policy and terms of use. Use
+					a strong and unique password for your account and avoid sharing sensitive information with anyone
+					you haven't thoroughly verified. If you encounter any suspicious activity or profiles, report them
+					to the site's customer support team.<br><br>
+				</dt>
+			</dl>
+		</div>
+	</div>
+
+
+	<?php include_once("footer.php"); ?>
